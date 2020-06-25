c14415c4e3c0e073aecdf39fe4168f014b097a8b
TheMisfit68 <janverrept@me.com>
2020-06-25 19:09:58 +0200

    Updated to swift SPM 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MilightDriver",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Wifi Bridge v6 Protocol.md"]),
         .testTarget(
             name: "MilightDriverTests",
             dependencies: ["MilightDriver"]),
