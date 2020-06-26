55e1dd0279d8bbdf425d925dfb387e9d9559b38f
TheMisfit68 <janverrept@me.com>
2020-06-27 00:38:15 +0200

    Better structured Package now

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MilightDriver",
-            dependencies: [],
-            exclude: ["Wifi Bridge v6 Protocol.md"]),
+            dependencies: []
+        ),
         .testTarget(
             name: "MilightDriverTests",
             dependencies: ["MilightDriver"]),
---
+++ ToDo.md
@@ -0,0 +1,1 @@
+# MilightDriver ToDo
